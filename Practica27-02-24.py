b093e6bbad00fd674db289afd1e45a0d8e87b545
pablohf7 <pablohf7@gmail.com>
2024-02-29 19:18:19 -0500

    Programa terminado

--- Practica27-02-24.py
+++ Practica27-02-24.py
@@ -1,6 +1,12 @@
 #PROGRAMA DE TASA DE IMPUESTO
-income=float(input("Intoduce el ingreso ananual: "))
+income=float(input("Intoduce el ingreso anual: "))
 
-if income<85518:
+if income<=85518:
     tax = income*0.18-556.02
+else:
+    tax = 14839.02 + (income-85518)*0.32
+tax=round(tax,0) 
+if tax<0:
+    tax=0
     
+print("El impuesto del contribuyente es igual a: ", tax)
